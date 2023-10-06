8fd110ac397f4fca00cd6003e60f15672b428321
shohruh223 <shohruh.abd2223@gmail.com>
2023-10-06 14:05:17 +0500

    add kurs

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,19 @@
 from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandStart
-
-from loader import dp
+from loader import dp, kurs
 
 
-@dp.message_handler(CommandStart())
-async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+@dp.message_handler(commands=['start'])
+async def send_wiki(message: types.Message):
+    await message.answer(f"Assalamu alaykum {message.from_user.full_name}! \n"
+                         f"Men Shohruh Abdurahmon tomonidan yaratilgan UZS-USD kurs botman\n"
+                         f"Sizni dollar kursi bo'yicha tanishtirib boraman.\n"
+                         f"Ushbu tugmani bosing 👉👉👉 '/dollar' ")
+
+
+@dp.message_handler(commands=['dollar'])
+async def send_wiki(message: types.Message):
+    try:
+        text = f"Bugungi kurs: 1AQSH dollar = {kurs} so'm"
+        await message.answer(text)
+    except:
+        await message.answer("noto'g'ri urunish")
--- loader.py
+++ loader.py
@@ -1,8 +1,17 @@
+# exchangerate-api.com
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
+import requests
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+
+API_KEY = "88fbb07fc673850d9416f2f1"
+currency='USD'
+url=f"https://v6.exchangerate-api.com/v6/{API_KEY}/pair/{currency}/UZS"
+
+response = requests.get(url)
+jsondata = response.json()
+kurs = response.json()['conversion_rate']
