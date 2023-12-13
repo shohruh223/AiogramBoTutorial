4f5e2172c3b65f35889078ce0a20c445f75943b7
shohruh223 <shohruh.abd2223@gmail.com>
2023-12-13 14:48:56 +0500

    add group

--- handlers/groups/start_group.py
+++ handlers/groups/start_group.py
@@ -1,11 +1,12 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
-from filters import IsPrivate
+from filters import IsGroup
 from loader import dp
 
 
-@dp.message_handler(IsPrivate(), CommandStart())
+@dp.message_handler(IsGroup(), CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!\n"
-                         f"bu shaxsiy yozishma")
+                         f"bu guruhdagi yozishma yozishma")
+
 
